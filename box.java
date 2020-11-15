92b6fc16ae949deaf11d9b8ce70c25759aab6388
tortoewka <guglpo4tavsedela@gmail.com>
2020-11-15 16:39:16 +0500

    сеттеры не успел сделать

---
+++ box.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class box<T > {
+
+    private T obj;
+
+    public box(T obj) {
+        this.obj = obj;
+    }
+
+    public T getObj() {
+        return obj;
+    }
+
+    public void setObj(T obj) {
+        this.obj = obj;
+    }
+
+    public void info() {
+        System.out.println("obj " + obj);
+        System.out.println("type " + obj.getClass());
+    }
+
+}
--- main.java
+++ main.java
@@ -1,18 +1,51 @@
-package jarvis;
+package com.company;
 
-public class java2 {
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main  {
+
+
+    public static void sortByArr(Integer a[]) {
+
+        Integer temp = a[0];
+        a[0] = a[1];
+        a[1] = temp;
+
+    }
+
+
+
 
 
     public static void main(String[] args) {
-        Cat c = new Cat("Barseek", 0, 0, 0);
-        dog d = new dog("sharik", 0, 0, 0);
+        fruit Orange = new fruit(1.0,5);
+        fruit Apple = new fruit(1.5,3);
+       box<Double> Box1 = new box(Orange);
+        box<Double> Box2 = new box(Apple);
 
-        c.jump(1);
-        d.jump(0.1);
-        c.run(100);
-        d.run(100);
-        c.sail(5);
-        d.sail(5);
 
-    }
-}
+
+
+        Integer[] arr;
+
+        arr = new Integer[2];
+        arr[0] = 1;
+        arr[1] = 2;
+        System.out.print(arr[0]);
+        System.out.println(arr[1]);
+        sortByArr(arr);
+        System.out.print(arr[0]);
+        System.out.println(arr[1]);
+
+        ArrayList<Integer> IntegersList  = new ArrayList<Integer>(Arrays.asList(arr));
+System.out.println(IntegersList);
+
+        System.out.println("вес корзины с яблоками " +  fruit.getWeight(Apple.getQuantity(),Apple.getVes()));
+        System.out.println("вес корзины с апельсинами " +   fruit.getWeight(Orange.getQuantity(),Orange.getVes()));
+
+
+
+}}
